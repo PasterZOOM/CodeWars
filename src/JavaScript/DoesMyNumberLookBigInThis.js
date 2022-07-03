4965105e6caa7c1b5966e3a8e1460874721f7e28
Ilya Pasternak <pasterzoom@gmail.com>
2022-07-03 12:14:26 +0300

    03.07.2022

--- src/JavaScript/DoesMyNumberLookBigInThis.js
+++ src/JavaScript/DoesMyNumberLookBigInThis.js
@@ -1,4 +1,7 @@
 const narcissistic = (value) =>
-    value === value.toString().split('').map(v => v ** value.toString().length).reduce((a, b) => a + b)
+    value === value.toString()
+        .split('')
+        .map(v => v ** value.toString().length)
+        .reduce((a, b) => a + b)
 
 console.log(narcissistic(153))
